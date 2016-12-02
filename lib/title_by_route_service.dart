0883e11fd777a207ef0ecc35c5cc6d91874324a1
ilikerobots <mike@hoolehan.com>
2016-12-02 18:44:33 +0200

    Rename package and init travis

--- lib/title_by_route_service.dart
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2016, Mike Hoolehan. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
-
-/// Support for updating document titles based on Route changes in Angular2.
-library title_by_route_service;
-
-export 'src/title_by_route_service_base.dart';
-
